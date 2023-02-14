070efd051dfca5245a236530dcac00d644482db6
AltonN <hyperg3nesis@gmail.com>
2023-02-14 06:02:57 -0800

    output to csv

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -1,42 +1,55 @@
 from bs4 import BeautifulSoup
 import requests
+from csv import writer
 NUM_PAGES = 3
 
 # Store user input as tag to search
 tag = input("Enter tag: ")
 
-# Loop through number of pages
-for page_num in range(1, NUM_PAGES+1):
+# Open file
+with open("../csv/" + tag + ".csv", "w", encoding="utf8") as file:
 
-    # Arrays to hold quotes and authors
-    quotes_text = []
-    authors = []
+    # Initialize writer
+    writer = writer(file)
 
-    # Store the URL as a string
-    url = "http://goodreads.com/quotes/tag/" + str(tag) + "?page=" + str(page_num)
+    # Write header
+    header = ["Quote", "Author"]
+    writer.writerow(header)
 
-    # Store the target webpage as a variable
-    page = requests.get(url)
+    # Loop through number of pages
+    for page_num in range(1, NUM_PAGES+1):
 
-    # Store the webpage html as a variable
-    soup = BeautifulSoup(page.text, "html.parser")
+        # Arrays to hold quotes and authors
+        quotes_text = []
+        authors = []
 
-    # List of quotes
-    quotes = soup.findAll("div", attrs={"class":"quoteText"})
+        # Store the URL as a string
+        url = "http://goodreads.com/quotes/tag/" + str(tag) + "?page=" + str(page_num)
 
-    # Parse list of quotes per page
-    for quote in quotes:
+        # Store the target webpage as a variable
+        page = requests.get(url)
 
-        # Count number of <br> tags
-        num_br = len(quote.find_all("br"))
+        # Store the webpage html as a variable
+        soup = BeautifulSoup(page.text, "html.parser")
 
-        # Only add quote if number of <br> tags is 1 or less
-        if num_br <= 1:
+        # List of quotes
+        quotes = soup.findAll("div", attrs={"class":"quoteText"})
 
-            # Add quote text and author to arrays
-            quotes_text.append(quote.find(string=True, recursive=False).strip())
-            authors.append(quote.find("span", attrs={"class":"authorOrTitle"}).find(string=True).strip())
+        # Parse list of quotes per page
+        for quote in quotes:
 
-    # Print quote followed by author
-    for quote, author in zip(quotes_text, authors):
-        print(quote + " -" + author)
+            # Count number of <br> tags
+            num_br = len(quote.find_all("br"))
+
+            # Only add quote if number of <br> tags is 1 or less
+            if num_br <= 1:
+
+                # Add quote text and author to arrays
+                quotes_text.append(quote.find(string=True, recursive=False).strip())
+                authors.append(quote.find("span", attrs={"class":"authorOrTitle"}).find(string=True).strip().rstrip(","))
+
+        # Write quote followed by author
+        for quote, author in zip(quotes_text, authors):
+            line = [quote, author]
+            writer.writerow(line)
+            
