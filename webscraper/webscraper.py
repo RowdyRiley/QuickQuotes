2d34216d63fe638f48a434fa2be20c4e328703e9
AltonN <hyperg3nesis@gmail.com>
2023-02-14 18:48:40 -0800

    refactored code

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -3,8 +3,40 @@ import requests
 from csv import writer
 NUM_PAGES = 3
 
+# Arrays to hold quotes and authors
+quotes = []
+authors = []
+
 # Store user input as tag to search
 tag = input("Enter tag: ")
+
+# Loop through number of pages
+for page_num in range(1, NUM_PAGES+1):
+
+    # Store the URL as a string
+    url = "http://goodreads.com/quotes/tag/" + str(tag) + "?page=" + str(page_num)
+
+    # Store the target webpage as a variable
+    page = requests.get(url)
+
+    # Store the webpage html as a variable
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    # List of html elements containing quotes
+    quote_objects = soup.findAll("div", attrs={"class":"quoteText"})
+
+    # Parse list of quote objects per page
+    for quote in quote_objects:
+
+        # Count number of <br> tags
+        num_br = len(quote.find_all("br"))
+
+        # Only add quote if number of <br> tags is 1 or less
+        if num_br <= 1:
+
+            # Add quote text and author to arrays
+            quotes.append(quote.find(string=True, recursive=False).strip())
+            authors.append(quote.find("span", attrs={"class":"authorOrTitle"}).find(string=True).strip().rstrip(","))
 
 # Open file
 with open("../csv/" + tag + ".csv", "w", encoding="utf8") as file:
@@ -16,40 +48,7 @@ with open("../csv/" + tag + ".csv", "w", encoding="utf8") as file:
     header = ["Quote", "Author"]
     writer.writerow(header)
 
-    # Loop through number of pages
-    for page_num in range(1, NUM_PAGES+1):
-
-        # Arrays to hold quotes and authors
-        quotes_text = []
-        authors = []
-
-        # Store the URL as a string
-        url = "http://goodreads.com/quotes/tag/" + str(tag) + "?page=" + str(page_num)
-
-        # Store the target webpage as a variable
-        page = requests.get(url)
-
-        # Store the webpage html as a variable
-        soup = BeautifulSoup(page.text, "html.parser")
-
-        # List of quotes
-        quotes = soup.findAll("div", attrs={"class":"quoteText"})
-
-        # Parse list of quotes per page
-        for quote in quotes:
-
-            # Count number of <br> tags
-            num_br = len(quote.find_all("br"))
-
-            # Only add quote if number of <br> tags is 1 or less
-            if num_br <= 1:
-
-                # Add quote text and author to arrays
-                quotes_text.append(quote.find(string=True, recursive=False).strip())
-                authors.append(quote.find("span", attrs={"class":"authorOrTitle"}).find(string=True).strip().rstrip(","))
-
-        # Write quote followed by author
-        for quote, author in zip(quotes_text, authors):
-            line = [quote, author]
-            writer.writerow(line)
-            
+    # Write quote followed by author
+    for quote, author in zip(quotes, authors):
+        line = [quote, author]
+        writer.writerow(line)
