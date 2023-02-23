9fee6bb77d69e1c523736f3734f0c1fac864cf5d
AltonN <hyperg3nesis@gmail.com>
2023-02-22 17:38:34 -0800

    Features: Get specific quote, Comments

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from dotenv import load_dotenv
 import psycopg2
 import os
@@ -16,7 +16,7 @@ conn = psycopg2.connect(url)
 # Initialize cursor
 cursor = conn.cursor()
 
-# Gets a random quote that user hasn't seen, adds it to seen list, returns quote
+# Get a random quote that user hasn't seen, add it to seen list, return quote and comments
 @app.get("/quote")
 def get_quote():
     with conn:
@@ -30,19 +30,97 @@ def get_quote():
 
             # Execute Query
             cursor.execute(QUERY)
-            results = cursor.fetchone()
+            quote = cursor.fetchone()
+            quote_id = quote[0]
 
-            # Define Query to insert quote user has seen
-            user_id = 1
-            quote_id = results[0]
-            QUERY2 = f'''INSERT INTO user_quotes_seen (user_id, quote_id)
-                        VALUES ({user_id}, {quote_id})'''
+            # Define Query to get comments associated with the quote
+            QUERY = f'''SELECT * FROM comments
+                        WHERE quote_id={quote_id};'''
 
             # Execute Query
-            cursor.execute(QUERY2)
+            cursor.execute(QUERY)
+            comments = cursor.fetchall()
 
-            # Form response
-            response = make_response(jsonify(results), 200)
+            # Define Query to insert quote user has seen
+            user_id = 1             # Placeholder until user profiles set up
+            quote_id = quote[0]
+            QUERY = f'''INSERT INTO user_quotes_seen (user_id, quote_id)
+                        VALUES ({user_id}, {quote_id});'''
+
+            # Execute Query
+            cursor.execute(QUERY)
+
+    # Form response
+    response = make_response(jsonify(quote, comments), 200)
     
-    # Success, return quote
+    # Success, return response
     return response
+
+# Get a specific quote, return quote and comments
+@app.get("/specific")
+def get_specific_quote():
+
+    # Request data from the frontend as a json
+    data = request.get_json()
+    quote_id = data["quote_id"]
+
+    with conn:
+        with conn.cursor() as cursor:
+
+            # Define Query to get a specific quote
+            QUERY = f'''SELECT * FROM quotes
+                        WHERE id={quote_id};'''
+
+            # Execute Query
+            cursor.execute(QUERY)
+            quote = cursor.fetchone()
+
+            # Define Query to get comments associated with the quote
+            QUERY = f'''SELECT * FROM comments
+                        WHERE quote_id={quote_id};'''
+
+            # Execute Query
+            cursor.execute(QUERY)
+            comments = cursor.fetchall()
+
+    # Form response
+    response = make_response(jsonify(quote, comments), 200)
+
+    # Success, return response
+    return response
+
+# Favorite a quote
+@app.put("/favorite")
+def favorite_quote():
+    return
+
+# Post comment
+@app.post("/comment")
+def post_comment():
+
+    # Request data from the frontend as a json
+    data = request.get_json()
+    content = data["content"]
+    quote_id = data["quote_id"]
+    user_id = data["user_id"]
+
+    with conn:
+        with conn.cursor() as cursor:
+
+            # Define Query to add comment
+            QUERY = f'''INSERT INTO "comments" (content, quote_id, user_id)
+                        VALUES ('{content}', {quote_id}, {user_id});'''
+
+            # Execute Query
+            cursor.execute(QUERY)
+
+    # Form response
+    response = make_response("Success", 201)
+
+    # Success, return response
+    return response
+
+# Put rating; Each user may only have 1 rating per quote
+@app.put("/rating")
+def put_rating():
+    return
