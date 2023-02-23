16e0c5f9dca0265c3570964ac3978e4bca9e137d
AltonN <hyperg3nesis@gmail.com>
2023-02-22 16:04:05 -0800

    Feature: Get random quote user hasn't seen

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from dotenv import load_dotenv
 import psycopg2
 import os
@@ -16,21 +16,33 @@ conn = psycopg2.connect(url)
 # Initialize cursor
 cursor = conn.cursor()
 
-# Test query, returns a random quote
-@app.get("/")
+# Gets a random quote that user hasn't seen, adds it to seen list, returns quote
+@app.get("/quote")
 def get_quote():
     with conn:
         with conn.cursor() as cursor:
 
-            # Define Query
-            QUERY = '''SELECT *
-                       FROM quotes
+            # Define Query to get random quote
+            QUERY = '''SELECT * FROM quotes
+                       WHERE id NOT IN (SELECT quote_id FROM user_quotes_seen)
                        ORDER BY random()
                        LIMIT 1;'''
 
             # Execute Query
             cursor.execute(QUERY)
-            results = cursor.fetchall()
+            results = cursor.fetchone()
+
+            # Define Query to insert quote user has seen
+            user_id = 1
+            quote_id = results[0]
+            QUERY2 = f'''INSERT INTO user_quotes_seen (user_id, quote_id)
+                        VALUES ({user_id}, {quote_id})'''
+
+            # Execute Query
+            cursor.execute(QUERY2)
+
+            # Form response
+            response = make_response(jsonify(results), 200)
     
     # Success, return quote
-    return results, 200
+    return response
