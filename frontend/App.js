5e55083d6c26eb350cc31ec212cf950ed7670835
Sam Hoyt <sphoyt@ucsc.edu>
2023-03-13 14:17:52 -0700

    Added login page and logout button

--- frontend/App.js
+++ frontend/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
+import { LoginScreen } from './src/screens/LoginScreen';
 import { QuoteFeedScreen } from './src/screens/QuoteFeedScreen';
 import { SubjectsScreen } from './src/screens/SubjectsScreen';
 import { SettingsScreen } from './src/screens/SettingsScreen';
@@ -15,6 +16,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="QuoteFeed" component={QuoteFeedScreen} />
         <Stack.Screen name="Subjects" component={SubjectsScreen} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
